6eb3ab4b4835603894a8440b0bd291e424644765
ManyaKha <60505955+ManyaKha@users.noreply.github.com>
2024-01-22 15:32:18 +0100

    Setting up database and API walking skeleton

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    //called each time that the datacontext is created
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users {get; set;}
+
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,6 +1,13 @@
-﻿namespace API;
+﻿namespace API.Entities;
 
 public class AppUser
 {
+    //List of the different necessary properties
+    //each property refers to a column in the database
+    public int Id {get; set;} //Part of built in conventions, Primary Key
+    public string FirstName { get; set;}
+    public string LastName {get; set;}
+    public string Email {get; set;}
+    public string Password { get; set;}
 
 }
